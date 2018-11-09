cb370694524b86d43aadce95bff6591504965772
Scott Harrison <github@scottharrisondesign.co.uk>
2018-11-09 23:44:18 +0000

    Keep track of selected brew once you click in brew list page

--- src/views/Brews.vue
+++ src/views/Brews.vue
@@ -2,7 +2,7 @@
     <div class="brews">
         <h1>Select A Brew</h1>
         <ul>
-            <li v-for="(brew, key, index) in $store.state.brews" :key="index"><router-link to="/flavours">{{ brew.name }}</router-link></li>
+            <li v-for="(brew, key, index) in $store.state.brews" :key="index"><router-link :to="{ name: 'flavours', params: { brew: brew.id }}">{{ brew.name }}</router-link></li>
             <li><router-link to="/flavours">New Brew</router-link></li>
         </ul>
     </div>
--- src/views/Flavours.vue
+++ src/views/Flavours.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="flavours">
     <h1>This is the flavours page</h1>
+    {{ $route.params.brew }} <!-- //TODO Need to save this value to the $store with 'currentBrew' or something as the key maybe? Then create a 'getBrew' method that we can pass this ID into and it gets the brew from the brews array -->
   </div>
 </template>
