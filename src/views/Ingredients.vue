20707c41f5abdc2504fafcae1b4ae3448b5a2864
Matt Miller <matt123miller@hotmail.co.uk>
2018-11-11 14:38:06 +0000

    Pleasing the compiler.

--- src/views/Ingredients.vue
+++ src/views/Ingredients.vue
@@ -2,7 +2,7 @@
     <div class="flavours">
         <h1>This is the flavours page</h1>
         <ul>
-        <li v-for="(ingredient, index) in availableIngredients" v-bind="index" :key="index">{{ ingredient }} </li>
+        <li v-for="(ingredient, index) in availableIngredients" v-bind="index" :key="index">{{ ingredient.name }} </li>
         </ul>
     </div>
 </template >
