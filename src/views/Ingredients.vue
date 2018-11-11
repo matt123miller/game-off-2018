d604051cc38d9313363b4d6ad61ff1492bbcbb0b
Matt Miller <matt123miller@hotmail.co.uk>
2018-11-11 00:31:09 +0000

    Made some Vue stuff render! Dayumn!

--- src/views/Ingredients.vue
+++ src/views/Ingredients.vue
@@ -1,15 +1,23 @@
 <template>
     <div class="flavours">
         <h1>This is the flavours page</h1>
-        <!-- <ul>
-        <li v-for="(ingredient, index) in $store.state.ingredients" :key="index">{{ ingredient.name }}</li>
-        </ul> -->
+        <ul>
+        <li v-for="(ingredient, index) in availableIngredients" v-bind="index" :key="index">{{ ingredient }} </li>
+        </ul>
     </div>
 </template >
 
 
 <script lang="ts">
-const a = "";
+import IngredientItem from "../components/IngredientItem";
+
+export default {
+  data() {
+    return {
+      availableIngredients: ["ingredient 1", "ingredient 2", "ingredient 3"]
+    };
+  }
+};
 </script>
 
 <style scoped lang='scss'>
