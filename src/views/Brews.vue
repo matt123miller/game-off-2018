27646878534b2110d3757a77472658022d564537
Scott Harrison <github@scottharrisondesign.co.uk>
2018-11-13 19:06:41 +0000

    going places...not sure where, but places

--- src/views/Brews.vue
+++ src/views/Brews.vue
@@ -3,10 +3,11 @@
         <h1>Select A Brew</h1>
         <ul>
             <li v-for="(brew, key, index) in $store.state.brews" :key="index"><router-link :to="{ name: 'ingredients', params: { brew: brew.id }}">{{ brew.name }}</router-link></li>
-            <li><router-link :to="{ name: 'ingredients', params: { id }}">New Brew</router-link></li>
+            <li><router-link :to="{ name: 'ingredients'}" v-on:click.native="createNewBrew(id)">New Brew</router-link></li>
         </ul>
-    </div>
+ {{allBrews}}   </div>
 </template>
+
 
 <script lang="ts">
 import { generateId } from "../components/utils";
@@ -14,8 +15,23 @@ import { generateId } from "../components/utils";
 export default {
   data() {
     return {
-        id: generateId()
+      id: generateId()
     };
+  },
+  methods: {
+    createNewBrew: function(id: string) {
+      // Create object, push to store.brews
+      const newBrew = {
+        id: id,
+        name: "New Brew" // Could add randomly generated name here in future?
+      };
+    //   this.$store.state.brews.push(newBrew);
+    }
+  },
+  computed: {
+    allBrews: function() {
+      return this.$store.state.brews;
+    }
   }
 };
 </script>
--- src/views/Ingredients.vue
+++ src/views/Ingredients.vue
@@ -1,24 +1,22 @@
 <template>
     <div class="flavours">
-        <h1>This is the flavours page {{ $route.params.brew }}</h1>
+        <h1>This is the flavours page</h1>
         <ul>
-        <li v-for="(ingredient, index) in availableIngredients" v-bind="index" :key="index">{{ ingredient.name }} </li>
+            <li v-for="(ingredient, index) in availableIngredients" :key="index">{{ ingredient.name }} </li>
+
         </ul>
     </div>
 </template >
 
 
 <script lang="ts">
-import { randomIngredients } from "../components/IngredientList";
+import { randomIngredients } from '../components/IngredientList';
 
 export default {
-  data() {
-    return {
-      availableIngredients: randomIngredients()
-    };
-  }
+    data() {
+        return {
+            availableIngredients: randomIngredients()
+        };
+    },
 };
 </script>
-
-<style scoped lang='scss'>
-</style>
