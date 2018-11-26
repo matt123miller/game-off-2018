8bfe4707961bdf509909f1df281423cfc9a391c2
Matt Miller <mmiller@tfpsark.com>
2018-11-26 13:00:55 +0000

    Moved ingredients back into state.

--- src/store.ts
+++ src/store.ts
@@ -6,20 +6,20 @@ import Brew from './interfaces/Brew';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    brews: Array<Brew>(),
-  },
-  mutations: {
-    addBrew(state, payload) {
-      state.brews.push(payload.brew);
+    state: {
+        brews: Array<Brew>(),
+        ingredients: Array<Ingredient>(),
     },
-    addIngredients(state, payload: Ingredient[]) {
-      // Get the current beer being developed...How?
+    mutations: {
+        addBrew(state, payload) {
+            state.brews.push(payload.brew);
+        },
+        addIngredients(state, payload: Ingredient[]) {
+            // Get the current beer being developed...How?
 
-      // Add the ingredients to that beer or the store
-      state.ingredients = payload;
+            // Add the ingredients to that beer or the store
+            state.ingredients = payload;
+        },
     },
-  },
-  actions: {},
-  ingredients: [],
+    actions: {},
 });
